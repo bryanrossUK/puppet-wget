bddd67449c16eaedd7822a151851d7f1059ddf9d
Bryan <bryan@liquidstate.net>
2010-06-14 00:47:11 +0100

    initial import

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+################################################################################
+# Class: wget
+#
+# This class will install wget - a tool used to download content from the web.
+#
+################################################################################
+class wget {
+	package { "wget": ensure => latest }
+}
+
+################################################################################
+# Definition: wget::fetch
+#
+# This class will download files from the internet.  You may define a web proxy
+# using $http_proxy if necessary.
+#
+################################################################################
+define wget::fetch($source,$destination) {
+	if $http_proxy {
+		exec { "wget-$name":
+			command => "/usr/bin/wget --output-document=$destination $source",
+			creates => "$destination",
+			environment => [ "HTTP_PROXY=$http_proxy", "http_proxy=$http_proxy" ],
+		}
+        } else {
+		exec { "wget-$name":
+			command => "/usr/bin/wget --output-document=$destination $source",
+			creates => "$destination",
+		}
+	}
+}
