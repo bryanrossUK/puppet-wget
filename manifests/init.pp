7689449b90224783b76f56766199ce88aa4bd134
John Cheng <github@johncheng.com>
2012-04-07 22:59:36 -0700

    setting to 2 space indentation http://docs.puppetlabs.com/guides/style_guide.html#spacing-indentation--whitespace ... also as excuse to play with github.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #
 ################################################################################
 class wget {
-	package { "wget": ensure => latest }
+  package { "wget": ensure => latest }
 }
 
 ################################################################################
@@ -16,16 +16,16 @@ class wget {
 #
 ################################################################################
 define wget::fetch($source,$destination) {
-	if $http_proxy {
-		exec { "wget-$name":
-			command => "/usr/bin/wget --output-document=$destination $source",
-			creates => "$destination",
-			environment => [ "HTTP_PROXY=$http_proxy", "http_proxy=$http_proxy" ],
-		}
-        } else {
-		exec { "wget-$name":
-			command => "/usr/bin/wget --output-document=$destination $source",
-			creates => "$destination",
-		}
-	}
+  if $http_proxy {
+    exec { "wget-$name":
+      command     => "/usr/bin/wget --output-document=$destination $source",
+      creates     => "$destination",
+      environment => [ "HTTP_PROXY=$http_proxy", "http_proxy=$http_proxy" ],
+    }
+  } else {
+    exec { "wget-$name":
+      command => "/usr/bin/wget --output-document=$destination $source",
+      creates => "$destination",
+    }
+  }
 }
